5dcec15ee0627c2a43d62fdc637224cf04dff2c9
Melike Kaya <112420252+melikkekaya@users.noreply.github.com>
2023-08-23 15:00:14 +0200

    apercu_final_scraping

--- src/apercu_complet.py
+++ src/apercu_complet.py
@@ -12,24 +12,31 @@ from bs4 import BeautifulSoup as bs
 import pdfplumber
 import io
 import re
-from pymongo import MongoClient
+import pymongo
 import os
 from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
+import numpy as np
 
-load_dotenv()
 
 ssl._create_default_https_context = ssl._create_unverified_context
 start_time = time.perf_counter()
 
-from sentence_transformers import SentenceTransformer
+load_dotenv()
+connection = os.getenv("MONGODB_URI")
+client = pymongo.MongoClient(connection, tlsCAFile=certifi.where())
+db = client["akkanto_db"]
+col = db["all_documents"]
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
-def embed(main_title, fr_text , nl_text ,model):
-    main_title_embedding = model.encode(main_title)
-    fr_text_embedding = model.encode(fr_text)
-    nl_text_embedding = model.encode(nl_text)
-    return main_title_embedding,fr_text_embedding,nl_text_embedding
+model = SentenceTransformer('LaBSE')
 
+def get_soup(url: str):
+    response = requests.get(url)
+    return bs(response.content, "html.parser")
+
+def embed(text, model):
+    text_embedding = model.encode(text)
+    return text_embedding
 
 def get_text_documet_parlementaire(url):
     rq = requests.get(url)
@@ -59,14 +66,46 @@ def get_text_documet_parlementaire(url):
 
     return fr_text, nl_text    
 
-# print("Connecting to database ...")
-
-# connection = 'mongodb+srv://maximberge:aAIbS7zRpsbsy6Gb@cluster0.p97p1.mongodb.net/?retryWrites=true&w=majority'
-
-# client = MongoClient(connection, tlsCAFile=certifi.where())
-# db = client["akkanto_db"]
-# collection = db["doc_complets_test"]
-
+def get_type(text):
+    if text == "Proposition De Loi":
+        return "Law Proposal"
+    elif text == "Projet De Loi":
+        return "Draft Legislation"
+    elif text == "Rapport":
+        return "Report"
+    elif text == "Proposition De Resolution":
+        return "Resolution Proposal"
+    elif text == "Proposition Reglement":
+        return "Regulation Proposal"
+    elif text == "Proposition Commiss. D'enquete":
+        return "Proposal for Commission of Enquiry"
+    elif text == "Proposition De Révision":
+        return "Revision Proposal"
+    elif text == "Presentation De Candidats":
+        return "Presentation of Candidates"
+    elif text == "Budget" or  text == "Elections":
+        return text
+    elif text == "Expose General":
+        return "General Overview"
+    elif text == "Tableaux Ou Listes":
+        return "Charts/Listes"
+    elif text == "Proposition De Declaration":
+        return "Declaration Proposal"
+    elif text == "Farde":
+        return "Binder"
+    elif text == "Decision Comm. De Concertation":
+        return "Concertation Commission Decision"
+    elif text == "Accord De Gouvernement":
+        return "Government Agreement"
+    elif text == "Liste Revision Constitution":
+        return "Constitutional Revision List"
+    elif text == "Feuilleton De Petitions":
+        return "Petitions List"
+    elif text == "Texte Adopte":
+        return "Text Adopted"
+    else:
+        return "Other"
+    
 
 def get_soup(url: str):
     response = requests.get(url)
@@ -82,7 +121,7 @@ group_document_urls = [base_url + link["href"] for link in group_links]
 
 print("Creating list of document data ...")
 document_list = []
-for group_document_url in group_document_urls[:1]:
+for group_document_url in group_document_urls:
     url_soup = get_soup(group_document_url)
     table = url_soup.find("table")
     document_links = table.find_all("a")
@@ -92,11 +131,10 @@ for group_document_url in group_document_urls[:1]:
         document_soup = get_soup(document)
         document_dict = {}
         section = document_soup.find("div", attrs={"id": "Story"})
-        
-        document_dict["title"] = section.find("h3").text.strip()
-        document_dict["document_number"] = document_dict["title"][-4:]
+        document_dict["fr_title"] = section.find("h3").text.strip()
+        document_dict["document_number"] = document_dict["fr_title"][-4:]
         document_dict["document_page_url"] = document
-        document_dict["main_title"] = section.find("h4").text.strip()
+        document_dict["fr_main_title"] = section.find("h4").text.strip()
         document_table = section.find("table")
         for rows in document_table.select("tr"):
             cells = rows.find_all("td", recursive=False)
@@ -110,87 +148,88 @@ for group_document_url in group_document_urls[:1]:
                 try:
                     document_dict[header] = rows.find("a").get("href")
                 except:
-                    document_dict[header] = content
+                    document_dict[header] = "NoURLfound"
             else:
                 document_dict[header] = content
-        final_dict = {}
-        final_dict["title"] = document_dict["title"]
-        final_dict["document_number"] = document_dict["document_number"]
 
-        if "Date de dépôt" in document_dict.keys():
-            final_dict["date"] = document_dict["Date de dépôt"]
+        existing_document = col.find_one({"document_number": document_dict["document_number"],"fr_source":"Documents Parlementaires Aperçu Complet"})
 
-        final_dict["document_page_url"] = document_dict["document_page_url"]
+        if existing_document:
+            print("Document with the same doc_number already exists.")
+            
+        else:
+            final_dict = {}
+            final_dict["fr_title"] = document_dict["fr_title"]
+            final_dict["document_number"] = document_dict["document_number"]
 
-        final_dict["main_title"] = document_dict["main_title"]
+            if "Date de dépôt" in document_dict.keys():
+                final_dict["date"] = document_dict["Date de dépôt"]
 
-        if "Document Chambre" in document_dict.keys(): 
-            final_dict["link_to_document"] = document_dict["Document Chambre"]
+            final_dict["document_page_url"] = document_dict["document_page_url"]
+            final_dict["fr_main_title"] = document_dict["fr_main_title"]
 
-        keywords = set()
-        if "Descripteur Eurovoc principal" in document_dict.keys():
-            keywords.add(document_dict["Descripteur Eurovoc principal"])    
-        if "Descripteurs Eurovoc" in document_dict.keys():
-            descripteurs = document_dict["Descripteurs Eurovoc"].split('|')
-            keywords.update(descripteurs)
-        if "Mots-clés libres" in document_dict.keys():
-            descripteurs = document_dict["Mots-clés libres"].split('|')
-            keywords.update(descripteurs)
-        final_dict["keywords"] = list(keywords)
+            if "Document Chambre" in document_dict.keys(): 
+                final_dict["link_to_document"] = document_dict["Document Chambre"]
 
-        final_dict["source"] = "Documents parlementaires aperçu complet"
+            keywords = []
+            if "Descripteur Eurovoc principal" in document_dict.keys():
+                keywords.append(document_dict["Descripteur Eurovoc principal"].title()) 
+            if "Descripteurs Eurovoc" in document_dict.keys():
+                descripteurs = document_dict["Descripteurs Eurovoc"].title().split('|')
+                keywords.extend(descripteurs)
+            if "Mots-clés libres" in document_dict.keys():
+                descripteurs = document_dict["Mots-clés libres"].title().split('|')
+                keywords.extend(descripteurs)
+            formatted_list = []
+            for item in keywords:
+                cleaned_item = item.strip()
+                if " " in cleaned_item:
+                    cleaned_item = cleaned_item.title()
+                else:
+                    cleaned_item = cleaned_item.capitalize()
+                formatted_list.append(cleaned_item)
+            final_dict["fr_keywords"] = list(set(formatted_list))
 
-        if "COMMISSION CHAMBRE" in document_dict.keys():
-            final_dict["commissionchambre"] = document_dict["COMMISSION CHAMBRE"]
-        if "1. COMMISSION CHAMBRE" in document_dict.keys():
-            final_dict["commissionchambre"] = document_dict["1. COMMISSION CHAMBRE"]
-        try:
-            fr_text, nl_text = get_text_documet_parlementaire(final_dict["link_to_document"])
-        except:
-            fr_text, nl_text = "", ""
-        final_dict["stakeholders"] = document_dict["Auteur(s)"]
-        final_dict["status"] = ""
+            final_dict["fr_source"] = "Documents Parlementaires Aperçu Complet"
 
-        main_title_embedding, fr_text_embedding, nl_text_embedding = embed(final_dict["main_title"], fr_text=fr_text, nl_text=nl_text, model=model)
-        final_dict["title_embedding"] = main_title_embedding
-        final_dict["fr_text_embedding"] = fr_text_embedding
-        final_dict["nl_text_embedding"] = nl_text_embedding
+            if "COMMISSION CHAMBRE" in document_dict.keys():
+                final_dict["commissionchambre"] = document_dict["COMMISSION CHAMBRE"]
+            if "1. COMMISSION CHAMBRE" in document_dict.keys():
+                final_dict["commissionchambre"] = document_dict["1. COMMISSION CHAMBRE"]
 
-        final_dict["topic"] = ""
-        if "commissionchambre" in final_dict.keys():
-            policy_level = final_dict["commissionchambre"].capitalize()
-            final_dict["policy_level"] = f'Federal Parliament ({policy_level})'
-        final_dict["type"] = document_dict["Type de document"]
-        final_dict["issue"] = ""
-        final_dict["reference"] = ""
+            if "commissionchambre" in final_dict.keys():
+                final_dict["commissionchambre"] = final_dict["commissionchambre"].title()
+            
+            if final_dict["link_to_document"] == "NoURLfound":
+                fr_text, nl_text = "",""
+                print("noURL")
+            else:
+                fr_text, nl_text = get_text_documet_parlementaire(final_dict["link_to_document"])
 
-       
-             
-        # query = {
-        # "$and": [
-        #     {"source": {"$eq": "Documents parlementaires aperçu complet"}},
-        #     {"document_number": {"$eq": final_dict['document_number']}}
-        # ]
-        # }
-      
+            final_dict["fr_stakeholders"] = document_dict["Auteur(s)"]
 
-        # if collection.find_one(query):
-        #     print("Checking if document already exist in database ...")  
-        #     print(f" The document with the number {final_dict['document_number']} already exists.")
-        # else:
-        #     print(f"Adding document with number {final_dict['document_number']} to the database ...")  
-        #     collection.insert_one(final_dict)  
-         
-      
-        # for item in document_list:
-        #     print(item)
-# print("Closing connection to database ...")
-# client.close()
+            final_dict["fr_title_embedding"] = embed(final_dict["fr_main_title"], model=model).tolist()
+
+            if final_dict["link_to_document"] == "NoURLfound":
+                final_dict["fr_text_embedding"] = np.array([0]*768).astype(np.float32).tolist()
+                final_dict["nl_text_embedding"] = np.array([0]*768).astype(np.float32).tolist()
+            else:
+                final_dict["fr_text_embedding"] = embed(fr_text, model=model).tolist()
+                final_dict["nl_text_embedding"] = embed(nl_text, model=model).tolist()
+
+            if "commissionchambre" in final_dict.keys():
+                policy_level = final_dict["commissionchambre"].title()
+                final_dict["policy_level"] = f'Federal Parliament ({policy_level})'
+
+            fr_type = re.sub(r'\d+', '', document_dict["Type de document"])
+            fr_type = ' '.join(word.capitalize() for word in fr_type.split())
+            final_dict["fr_type"] = get_type(fr_type)
+
+            print(f"Adding data {index} into database..")
+
+            col.insert_one(final_dict)        
+            
 
 end_time = time.perf_counter()
 print(round(end_time - start_time, 2), "seconds")
 
-
-
-# with open("data/apercu_complet_partial.json", "w") as fout:
-#     json.dump(document_list, fout,ensure_ascii=False)
