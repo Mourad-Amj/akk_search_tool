b2eef54fc6c7e9873fdd0be40bc08f0aed759e5e
Gregoire Hupin <hupingregoire@hotmail.com>
2023-08-24 15:01:01 +0200

    Testing reset button. Adding session_states everywhere

--- proof_of_concept/app_test.py
+++ proof_of_concept/app_test.py
@@ -6,13 +6,159 @@
 import streamlit as st
 import pandas as pd
 import io
-
+from streamlit_tags import st_tags, st_tags_sidebar
 from sentence_transformers import SentenceTransformer, util
 import datetime
 
-
+st.set_page_config(page_title = "lachambre search engine")
 # initialization
 st.title("PoC: lachambre.be custom search engine")
+
+# Some testing data 
+
+@st.cache_data
+def load_agenda():
+    database=[{'r': """Banking
+Services""",
+            'level': """Federal
+Parliament
+(Plenary
+Session)""",
+            'type': """Law Proposal """,
+            'issue': """Proposition de loi modifiant le Code de droit
+économique en ce qui concerne la garantie
+du service bancaire de base aux travailleurs
+migrants. (3345)""",
+            'date': """17/05/2023""",
+            'authors': ["Ben Segers (Vooruit)"],
+            'url': "https://www.lachambre.be/emeeting/?organ=plen&status=final&date=2023-05-17&number=1",
+            'status': """Will be taken
+into
+consideration"""
+            }, {'r': """Banking Sector""",
+            'level': """Walloon
+Parliament
+(Plenary
+Session)""",
+            'type': """ Vote on
+motions""",
+            'issue': """- La dégradation de l'accès à l'argent
+liquide
+- Les incertitudes pesant sur l'accès aux
+distributeurs bancaires
+- La responsabilité du Gouvernement
+wallon dans la réduction excessive du
+nombre de distributeurs de billets""",
+            'date': """17/05/2023""",
+            'authors': ["""John Beugnies
+(PTB)
+Benoit Dispa (Les
+Engagés)
+André Antoine (Les
+Engagés)
+Eric Lomba (PS)
+Sybille de Coster-
+Bauchau (MR)
+Rodrigue Demeuse
+(Ecolo)"""],
+            'url': "http://nautilus.parlement-wallon.be/Archives/2022_2023/ODJS/odjs20230517.pdf",
+            'status': """Will be voted
+on."""
+            }, {'r': """Sustainable
+Mobility &
+Environment""",
+            'level': """Walloon
+Parliament
+(Environment
+Committee)""",
+            'type': """ Oral
+Question""",
+            'issue': """La réutilisation et le recyclage des éléments -
+des voitures électriques""",
+            'date': """16/05/2023""",
+            'authors': ["""François Bellot
+(MR)
+Walloon Minister
+of Environment,
+Celine Tellier
+(Ecolo)"""],
+            'url': "http://nautilus.parlement-wallon.be/Archives/2022_2023/ODJC/odjc170.pdf",
+            'status': """"""
+            }, {'r': """Home Loan""",
+            'level': """Walloon
+Parliament""",
+            'type': """Oral
+Question""",
+            'issue': """L'impact de la hausse des taux d'intérêt des
+crédits hypothécaires sur l'accès à la propriété""",
+            'date': """16/05/2023""",
+            'authors': ["Jaqueline Galant (MR)"],
+            'url': "http://nautilus.parlement-wallon.be/Archives/2022_2023/ODJC/odjc169.pdf",
+            'status': """"""
+            }, {'r': """Energy
+Transition""",
+            'level': """Walloon
+Parliament
+(Energy
+Committee)""",
+            'type': """ Oral
+Question""",
+            'issue': """Le manque d’actions pour relever les défis
+de la transition énergétique en Wallonie""",
+            'date': """16/05/2023""",
+            'authors': ["""François
+Desquesnes (Les
+Engagés)
+Walloon Minister
+of Energy and
+Climate, Philippe
+Henry (Ecolo)"""],
+            'url': "http://nautilus.parlement-wallon.be/Archives/2022_2023/ODJC/odjc164.pdf",
+            'status': """"""
+            }, {'r': """Green
+Certificates""",
+            'level': """Walloon
+Parliament
+(Energy
+Committee)""",
+            'type': """ Oral
+Question""",
+            'issue': """Les mesures de soutien au secteur de la
+biométhanisation malgré la réduction de
+l'enveloppe des
+certificats verts""",
+            'date': """16/05/2023""",
+            'authors': ["""Rachel Sobry (MR)
+Walloon Minister
+of Energy and
+Climate, Philippe
+Henry (Ecolo)"""],
+            'url': "http://nautilus.parlement-wallon.be/Archives/2022_2023/ODJC/odjc164.pdf",
+            'status': """"""
+            }, {'r': """Energy
+Transition""",
+            'level': """Walloon
+Parliament
+(Energy
+Committee)""",
+            'type': """Oral
+Question""",
+            'issue': """Les perspectives de création d'emplois liées -
+à la transition énergétique""",
+            'date': """16/05/2023""",
+            'authors': ["""Eddy Fontaine (PS)
+Walloon Minister
+of Energy and
+Climate, Philippe
+Henry (Ecolo)"""],
+            'url': "http://nautilus.parlement-wallon.be/Archives/2022_2023/ODJC/odjc164.pdf",
+            'status': """"""
+            }]
+    
+    embedder = SentenceTransformer('all-MiniLM-L6-v2')
+    for item in database:
+        item["embedding"] = embedder.encode(item["issue"], convert_to_tensor=False)
+    return database
 
 @st.cache_data
 def load_data():
@@ -81,8 +227,9 @@ def load_data():
         item["embedding"] = embedder.encode(item["text"], convert_to_tensor=False)
     return database
 
-@st.cache_data
-def apply_topic_filter(database, score_threshold):
+# Filtering functions
+
+def apply_topic_filter(database, query_embedding, score_threshold):
     search_result = []
     for item in database: 
         cos_score = util.cos_sim(item["embedding"], query_embedding)[0]
@@ -101,66 +248,163 @@ def apply_date_filter(database, start_date, end_date):
         else:
             search_result.append(item)
     return search_result
-    
-# ---
+
+@st.cache_data    
+def apply_keywords_filter(database, keywords, language):
+    result = []
+    if not language in ['fr', 'nl']:
+        print(f"{language} is not a valid input. Make sure language is fr or nl.")
+        return None
+    for item in database:
+        try:
+            if all(keyword in item[f"{language}_keywords"] for keyword in keywords):
+                result.append(item)
+        except:
+            print(f"Error in applying the keywords filter. Do all database items have a {language}_keywords tag?")
+    return result
+            
+# ----------------------------------------------------------------------
 # Load environment
-# ---
+# ----------------------------------------------------------------------
+
+agenda = load_agenda()
 database = load_data()
-# ---
+
+# ----------------------------------------------------------------------
 # Parameters
-# ---
-
-language = st.radio(
-    "Output language: ",
-    ('fr', 'nl'))
+# ----------------------------------------------------------------------
 
 
-# ---
+# Language setting
+
+if "language" not in st.session_state:
+    st.session_state.language = "fr"
+st.session_state.language = st.radio(
+    "Language: ",
+    ('fr', 'nl')
+    )
+
+# Reset button
+
+if st.button('Reset filters'):
+    for key in st.session_state.keys():
+        del st.session_state[key]
+
+# ----------------------------------------------------------------------
+### Filters sub-section
+# ----------------------------------------------------------------------
+
+st.subheader("Filters")
+col1, col2, col3= st.columns(3)
+
 # Search filter
-# ---
 
-# Text field + processing query
+if 'search_text' not in st.session_state:
+    st.session_state['search_text'] = 'Type your search'
+st.session_state['search_text'] = st.text_input(st.session_state['search_text'])
 
-# Slider to tune the threshold on cos similarity
-score_threshold = st.slider('Filtering threshold: ', 0.0, 0.5, 0.35)
-st.write("Cosine similarity set at", score_threshold)
-
-# Date filter 
-start_date = st.date_input('Start date', datetime.date.today() - datetime.timedelta(days=15))
-end_date = st.date_input('End date', datetime.date.today())
-if start_date < end_date:
-    st.success('Start date (default: 2 weeks ago): `%s`\n\nEnd date (default: today):`%s`' % (start_date, end_date))
-else:
-    st.error('Error: End date must fall after start date.')
+with col1:
+    # Slider to tune the threshold on cos similarity
+    if 'score_threshold' not in st.session_state:
+        st.session_state['score_threshold'] = 0.35
+    st.session_state['score_threshold'] = st.slider('Filtering threshold: ', 0.0, 0.5, st.session_state['score_threshold'])
+    st.write("Cosine similarity set at", score_threshold)
+    st.info('Filter by relevance', icon="ℹ️")
     
+# Date filter 
+with col2:
+# Date filter 
+    #st.date_input("test date",format="DD/MM/YYYY")
+
+    # Initializing default dates settings. Runs once.
+    if 'start_date' not in st.session_state:
+        st.session_state['start_date'] = datetime.date.today() - datetime.timedelta(days=730)
+    if 'end_date' not in st.session_state:
+        st.session_state['end_date'] = datetime.date.today()
+    # Creating the widgets.    
+    start_date = st.date_input('Start date', st.session_state['start_date'], format="DD/MM/YYYY")
+    end_date = st.date_input('End date', st.session_state['end_date'], format="DD/MM/YYYY")
+    if start_date < end_date:
+        st.success('Start date (default: 2 weeks ago): `%s`\n\nEnd date (default: today):`%s`' % (start_date, end_date))
+    else:
+        st.error('Error: End date must fall after start date.')
+
+    # Update session state when dates are changed
+    if start_date != st.session_state['start_date']:
+        st.session_state['start_date'] = start_date
+    if end_date != st.session_state['end_date']:
+        st.session_state['end_date'] = end_date
+
+with col3:
+    eurovocs_list = []
+    keywords = st_tags(
+        label="Enter EuroVocs keywords",
+        text ="Press enter to add more",
+        suggestions=eurovocs_list,
+        key = "keywords_filter"
+    )
 # Applying the filters
 
 embedder = SentenceTransformer('all-MiniLM-L6-v2')
-search = st.text_input('Type your search')
-query_embedding = embedder.encode(search, convert_to_tensor=False)
 
-search_result = apply_topic_filter(apply_date_filter(database, start_date, end_date), score_threshold)
+print(st.session_state['search_text'])
+query_embedding = embedder.encode(st.session_state['search_text'], convert_to_tensor=False)
+
+agenda_search = apply_date_filter(agenda, start_date, end_date)
+agenda_search = apply_topic_filter(agenda_search, query_embedding, score_threshold)
+
+doc_search = apply_date_filter(database, start_date, end_date)
+if not st.session_state['search_text'] == 'Type your search':
+    query_embedding = embedder.encode(st.session_state['search_text'], convert_to_tensor=False)
+    agenda_search = apply_topic_filter(agenda_search, query_embedding, score_threshold)
+    doc_search = apply_topic_filter(doc_search, query_embedding, score_threshold)
+
+if keywords:
+    doc_search = apply_keywords_filter(doc_search, keywords, st.session_state.language)
+
+# -----------------------------------------------------------------------
+### Converting databases to dataframes
+# -----------------------------------------------------------------------
+try:
+    agenda_temp = pd.DataFrame(agenda_search)
+    agenda_out = agenda_temp[['r', 'level', 'type', 'issue', 'date', 'authors', 'url', 'status']]
+except:
+    agenda_out = pd.DataFrame(agenda_search)
 
 try:
-    df_temp = pd.DataFrame(search_result)
-    df_new = df_temp[['id', 'title', 'author', 'date', 'source', 'text']]
+    df_temp = pd.DataFrame(doc_search)
+    df_out = df_temp[['id', 'title', 'author', 'date', 'source', 'text']]
 except:
-    df_new = pd.DataFrame(search_result)
+    df_out = pd.DataFrame(doc_search)
 
-st.dataframe(df_new, hide_index=True, use_container_width= True)
 
+# -----------------------------------------------------------------------
+### Displaying the dataframes
+# -----------------------------------------------------------------------
+st.write("Agenda: ")
+try:
+    agenda_out = st.data_editor(agenda_out, hide_index=True, num_rows = "dynamic", use_container_width= True)
+except:
+    st.write("Empty agenda")
+st.write("Looking back: ")
+try:
+    df_out = st.data_editor(df_out, hide_index=True, num_rows = "dynamic", use_container_width= True)
+except:
+    st.write("Empty log")
+if "agenda_df" not in st.session_state:
+    st.session_state['agenda_df'] = pd.DataFrame(columns=['r', 'level', 'type', 'issue', 'date', 'authors', 'url', 'status'])
 
 if "output_df" not in st.session_state:
     st.session_state['output_df'] = pd.DataFrame(columns=['id', 'title', 'author', 'date', 'source', 'text'])
 
-# ---
-# Adding search result
-# ---
 
 if st.button("Append search result"):
     # update dataframe state
-    st.session_state.output_df = pd.concat([st.session_state.output_df, df_new], axis=0, ignore_index=True).drop_duplicates(subset='id', keep="last")
+    st.session_state.agenda_df = pd.concat([st.session_state.agenda_df, agenda_out], axis=0, ignore_index=True).drop_duplicates(subset='issue', keep="last")
+    st.session_state.output_df = pd.concat([st.session_state.output_df, df_out], axis=0, ignore_index=True).drop_duplicates(subset='id', keep="last")
+
     
     st.text("Updated dataframe")
+
 
 
